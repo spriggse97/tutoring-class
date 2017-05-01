d839eea92f76baea5fa4b221a262e280ed8510fe
spriggse97 <eianspriggs97@gmail.com>
2017-05-01 15:49:50 -0700

    added java script

---
+++ justingcase/js/simple.js
@@ -0,0 +1,47 @@
+"name".length;
+//this is counting the letters
+//comments
+confirm("are you happy?");
+//this is for yes and no questions
+prompt("what do you want to learn");
+// this is asking for information
+// information is seen in 3 main ways
+//1 numbers simple
+var numbs = 4-6;
+var numbs =3+4;
+var numbs =1 * 2;
+// string its information js doesnt understands what is can see if it already exists
+var stringslength ="name".length;
+
+// booleans  aka true or falce
+var yesOrNo ="I'm coding like a champ".length > 10;
+// testing methods
+console.log("Hello");
+console.log(2 * 5);
+//
+var mynamelength = "eian".length;
+if (mynamelength < 5 ) {
+    console.log( mynamelength);
+}
+
+if ("eian" ==="Eian" )
+{
+    console.log("if the exprestion was true");
+}
+else
+{
+    console.log("computers will never make asumptions");
+
+
+}
+//if (10 = 10) this does not mean 10 equils 10
+if (10 === 10){
+    console.log("You got a true!");
+} else{
+    console.log("You got a false!");
+}
+// == means similar === means exact
+
+//modulo aka %
+//the computer will divide the first number by the second, and then return the remainder of that division.
+//So if we do 23 % 10, we divide 23 by 10 which equals 2 with 3 left over. So 23 % 10 evaluates to 3.
